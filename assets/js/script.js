61cf509d9bf5cedb93b2bc3aae53b99d5b8269de
Bartek Bartosik <b.bartek987@gmail.com>
2023-08-23 15:14:15 +0200

    adding deaspoering menu after click

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ const hamMenu = document.querySelector(".hamburger-menu");
 hamMenu.addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
+
+// close sideBar after click
+
+const allMenuLink = document.querySelectorAll(".menu-link");
+
+allMenuLink.forEach(function (link) {
+  link.addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+  });
+});
